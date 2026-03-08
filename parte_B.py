137d91dc19f6241d9770500b0544ed0b02394b71
andrearojas898 <est.andrea.rojasa@unimilitar.edu.co>
2026-03-07 21:57:12 -0500

    Update parte_B.py

--- parte_B.py
+++ parte_B.py
@@ -7,12 +7,12 @@ n = nmp.arange(0,9) #crea vector
 x1 = nmp.cos(2*nmp.pi*100*n*Ts)  #definicion de señales
 x2 = nmp.sin(2*nmp.pi*100*n*Ts)
   #si la señal se invierte se puede calcular la correlacion como una convolucion 
-x2_inv = x2[::-1]
+X = x2[::-1]
    # ya con la señal invertida, hallar correlación
-R = nmp.convolve(x1, x2_inv)
+R = nmp.convolve(x1, X)
 print("Correlacion =", R)
 
-k = nmp.arange(-8,9) #Desplazamientos de la señal
+k = nmp.arange(-8,8) #Desplazamientos de la señal
 plt.stem(k,R) #grafica de correlacion
 
 plt.title("Correlacion cruzada entre x1 y x2")
